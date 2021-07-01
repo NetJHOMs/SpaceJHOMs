3478f63c79c5a405546ff7c3e1e9c8eb7b3d2144
Christian Elías <contacto@christianecg.com>
2021-07-01 15:40:30 -0500

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Space JHOM's
+
+This is a website that contains related info about SpaceX missions, with the main propouse to divulgate some cosmos-related data.
+
+## Usage
+
+Avaiable to use at: https://netjhoms.github.io/SpaceJHOMs
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ assets/script.js
@@ -0,0 +1,21 @@
+window.onload = async () => {
+	const app = document.getElementById('app');
+
+	let spaceXData = await fetch('/data.json');
+	spaceXData = await spaceXData.json();
+	console.log(spaceXData);
+
+	let missions = spaceXData.map((mission) => {
+		return `
+			<article>
+				<h1>${mission.mission_name}</h1>
+				<p>${mission.description}</p>
+				<img src='${mission.image}' />
+				<a href='${mission.website}'>Web site</a><br>
+				<a href='${mission.wikipedia}'>Wikipedia</a>
+			</article>
+		`;
+	});
+
+	missions.forEach((mission) => (app.innerHTML += mission));
+};
